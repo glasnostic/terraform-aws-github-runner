8eb655aadda1de60c1c3c86255c4f4fcd9bdba93
Christoph Kassen <christoph@glasnostic.com>
2022-03-11 17:42:09 +0100

    Daemonize properly

--- images/run-bazel-cache.sh
+++ images/run-bazel-cache.sh
@@ -4,8 +4,8 @@ mkdir -p /opt/bazel-cache
 chown -R 1000:1000 /opt/bazel-cache
 chmod -R 777 /opt/bazel-cache
 
-docker run -u 1000:1000 -v /opt/bazel-cache:/data \
+docker run -d -u 1000:1000 -v /opt/bazel-cache:/data \
    -p 9090:8080 -p 9092:9092 buchgr/bazel-remote-cache \
    --s3.auth_method=iam_role \
    --s3.bucket=glasnostic-bazel-build-cache --s3.endpoint=s3.us-west-2.amazonaws.com \
-   --max_size=150 &
+   --max_size=50
