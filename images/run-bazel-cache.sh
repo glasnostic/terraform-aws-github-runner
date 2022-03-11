267a2c2fd184faba70d7684626ea8c697ece81cb
Christoph Kassen <christoph@glasnostic.com>
2022-03-11 17:44:38 +0100

    Set additional s3 parameters

--- images/run-bazel-cache.sh
+++ images/run-bazel-cache.sh
@@ -7,5 +7,6 @@ chmod -R 777 /opt/bazel-cache
 docker run -d -u 1000:1000 -v /opt/bazel-cache:/data \
    -p 9090:8080 -p 9092:9092 buchgr/bazel-remote-cache \
    --s3.auth_method=iam_role \
-   --s3.bucket=glasnostic-bazel-build-cache --s3.endpoint=s3.us-west-2.amazonaws.com \
+   --s3.iam_role_endpoint=http://169.254.169.254 --s3.region=us-west-2 \
+   --s3.bucket=glasnostic-bazel-build-cache \
    --max_size=50
